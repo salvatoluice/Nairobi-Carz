28e8e3128ef2d6f4d6b1d19572b895ca250a19b3
Salvato Luice <salvatoluice5@gmai.com>
2023-01-30 11:01:18 +0300

    Change not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div style={{height: '70vh', alignItems: 'center', textAlign: 'center', margin: 'auto'}}>
+  return <div style={{height: '36vh', alignItems: 'center', textAlign: 'center', margin: 'auto'}}>
     <h1 style={{paddingTop: '5rem'}}>Oops! NotFound</h1>
     <h2 style={{paddingTop: '5rem'}}>You might just be on the wrong page.</h2>
   </div>
