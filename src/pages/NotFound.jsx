5347b1c17e2ff6a4eda2ff6c996fd585defa104b
salvatoluice <salvatoluice5@gmail.com>
2023-03-11 04:32:37 -0800

    Make Changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div style={{height: '50vh', alignItems: 'center', textAlign: 'center', margin: 'auto'}}>
+  return <div style={{height: 'auto', alignItems: 'center', textAlign: 'center', margin: 'auto'}}>
     <h1 style={{paddingTop: '5rem'}}>Feature Coming Soon!!</h1>
     <h2 style={{paddingTop: '5rem'}}>Still in Develpoment, Stay tuned.</h2>
-    <h3 style={{paddingTop: '5rem'}}>Have a great day as you navigate through this amazing web page😎😎</h3>
+    <h3 style={{paddingTop: '5rem'}}>Hang on Tight, we gat you!😎😎</h3>
   </div>
 };
 
